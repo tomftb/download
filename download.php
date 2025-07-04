ee1d739ac966c53657747cb17a99b239ee0a1c81
tomftb <tborczynski87@gmail.com>
2025-07-04 21:47:17 +0200

    add count

--- download.php
+++ download.php
@@ -12,7 +12,16 @@ class DownloadFile
     private string $temporaryFileDirectory='';
     private DOMDocument $dom;
     private string $logFile='';
-	
+    private int $filesDownloaded = 0;
+    private int $filesAvailable = 0;
+    private int $downloadIterator = 0;
+    private string $filesURL = '';
+    private string $quality = '';
+    private string $fileList = '';
+    private bool $statusFileListToDelete = false;
+    private array $toDelete = [];
+    private \CurlHandle $curlHandle;
+    
     public function __construct()
     {
         $this->uniqid = uniqid();
@@ -44,6 +53,7 @@ class DownloadFile
 
 	$body = $response->getBody("<script>");
 	self::readDOM($body);
+        self::log(__METHOD__."() FILES DOWNLOADED - ".strval($this->filesDownloaded));
     }
 
     public function download2(?array $argv=[])
@@ -51,7 +61,7 @@ class DownloadFile
         self::log(__METHOD__."()");
         if($argv === null){
             self::log(__METHOD__."() SCRIPT ERROR:".PHP_EOL.": argv === null");          
-           exit();
+            exit();
 	}
 	if(!array_key_exists(1,$argv)){
             self::log(__METHOD__."() SCRIPT ERROR:".PHP_EOL." SET URL AS AN ARGUMENT OF SCRIPT");
@@ -75,36 +85,33 @@ class DownloadFile
 	}
 	curl_close($ch);
     }
-	
+
     private function readDOM(string $content='')
     {
         self::log(__METHOD__."()");
 		@$this->dom->loadHTML($content);
 		$title = self::getPageTitle();
-
 		self::readScripts($title);
-		
-
 	}
 
-        function getPageTitle():string
-        {
-            try {
-                self::log(__METHOD__."()");
-                $title = $this->dom->getElementsByTagName('title')->item(0)->nodeValue;
-                //echo $title."\r\n";
-                $titleClean = preg_replace('/[^a-zA-Z0-9\s]/', '', $title);
-                return $titleClean;
-                //echo $titleClean; // Output: "Hll This is a tring with spcil chracters"
-                
-            } catch (Exception $e) {
-                echo "Error: " . $e->getMessage();
-                return '';
-            }
+    function getPageTitle():string
+    {
+        try {
+            self::log(__METHOD__."()");
+            $title = $this->dom->getElementsByTagName('title')->item(0)->nodeValue;
+            //echo $title."\r\n";
+            $titleClean = preg_replace('/[^a-zA-Z0-9\s]/', '', $title);
+            return $titleClean;
+            //echo $titleClean; // Output: "Hll This is a tring with spcil chracters"            
+        }
+        catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
             return '';
         }
- 
-	private function readScripts(string $title='')
+        return '';
+    }
+
+    private function readScripts(string $title='')
 	{
             self::log(__METHOD__."()");
             $found = false;
@@ -222,119 +229,128 @@ class DownloadFile
         self::log(__METHOD__."() URL:".PHP_EOL.$m3u8);
         $quality = explode('.',$downloadFileParts);
         self::log(__METHOD__."() QUALITY[0]:".PHP_EOL.$quality[0]);
-        $filesURL = substr($m3u8, 0,-8);
-        self::log(__METHOD__."() FILES URL:".PHP_EOL.$filesURL);
+        $this->quality = $quality[0];
+        $this->filesURL = substr($m3u8, 0,-8);
+        self::log(__METHOD__."() FILES URL:".PHP_EOL.$this->filesURL);
         $curlError = false;
-        $i = 1;
         /*
          * CREATE FILE LIST
          */
-        $fileList = $this->temporaryFileDirectory."list.txt";
+        $this->fileList = $this->temporaryFileDirectory."list.txt";
         /*
-         * FILE TO DELETE
+         * Initialise cUrl
          */
-        $toDelete=[];
-        while($curlError !== true){
+        self::initCurl();
+        try{
             /*
-             *  Initialize cURL
+             * COUNT FILES
              */
-            $ch = curl_init();
-            $fileName = $quality[0].strval($i++).".ts";
-            $destination = $this->temporaryFileDirectory.$fileName;
-            $url = $filesURL.$fileName;
-            self::log(__METHOD__."() FILE URL:".PHP_EOL.$url);
-            /*
-             *  Set cURL options
-             */
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return the transfer as a string
-            curl_setopt($ch, CURLOPT_HEADER, 0); // Don't return the header
-            /*
-             *  Optional: Set a timeout for the download (in seconds)
-             */
-            curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-            /*
-             *  Optional: Follow redirects
-             */
-            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-            /*
-             *  Optional: User-Agent header (some servers require this)
-             */
-            curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
-
-            // Optional: Handle SSL certificate issues (use with caution, better to fix certificate problems)
-            // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-
-            /*
-             * Execute cURL and get the content
-             */
-            $fileContent = curl_exec($ch);
-
-            $info = curl_getinfo($ch);
-
-            if (curl_errno($ch)) {
-                self::log(__METHOD__."() cURL ERROR:".PHP_EOL.curl_error($ch));
-                $curlError = true;
-                break;
+            while($curlError !== true){
+                self::countContent($curlError);
             }
-            if($info['http_code']!==200){
-                //echo "cURL Error http_code " . $info['http_code'] . "\r\n";
-                self::log(__METHOD__."() cURL HTTP CODE ERROR:".PHP_EOL.$info['http_code']);
-                $curlError = true;
-                break;
+            //echo $this->filesAvailable;
+            $this->downloadIterator = 0;
+            $curlError = false;
+            /*
+             * DOWNLOAD CONTENT
+            */
+            while($curlError !== true){
+                self::downloadContent();
             }
-
-            if ($fileContent === false || empty($fileContent)) {
-                self::log(__METHOD__."() cURL CONTENT ERROR:".PHP_EOL."No content received for {$url}. It might be an empty file or a download failure.");
-                $curlError = true;
-                break;
-            }
-
-            $bytesWritten = file_put_contents($destination, $fileContent);
-
-            if ($bytesWritten !== false) {
-                self::log(__METHOD__."()".PHP_EOL."Successfully downloaded {$url}".PHP_EOL."Saved to {$destination} ({$bytesWritten} bytes).");
-                $fileListToDelete = self::createFFMPEGList($fileList,$destination,$toDelete);
-            }
-            else {
-                self::log(__METHOD__."()Error saving file:".PHP_EOL." {$destination}\r\n");
-            }
-            $toDelete[]=$destination;
-
-            curl_close($ch);
-
         }
-        self::log(__METHOD__."() MERGE");
-        $command = "ffmpeg -loglevel quiet -f concat -safe 0 -i ".escapeshellarg($fileList)." -c copy ".escapeshellarg($this->completeFileDirectory.$title."_".$this->uniqid.".mp4");
+        catch(\Exception $e){
+            self::log($e->getMessage());
+        }
+        curl_close($this->curlHandle);
+        self::log(__METHOD__."() MERGE DOWNLOADED FILES");
+        $command = "ffmpeg -loglevel quiet -f concat -safe 0 -i ".escapeshellarg($this->fileList)." -c copy ".escapeshellarg($this->completeFileDirectory.$title."_".$this->uniqid.".mp4");
         //$command = "ffmpeg -f concat -safe 0 -i ".escapeshellarg($fileList)." -c copy ".escapeshellarg($downloadCompleteDirectory.DIRECTORY_SEPARATOR.$title."_".$this->uniqid.".mp4");
         $output = shell_exec($command);
 
         self::log(__METHOD__."() FFMPEG".PHP_EOL.$output);
 
-        if($fileListToDelete){
-            $toDelete[]=$fileList;
+        if($this->statusFileListToDelete){
+            $this->toDelete[]=$this->fileList;
         }
         /*
          * CLEAN
          */
-        self::clean($toDelete);
+        self::clean();
     }
-    
-    private function createFFMPEGList(string $fileListPath='', string $filennamepart='')
+
+    private function countContent(bool &$curlError=false)
     {
-        $result = file_put_contents($fileListPath, "file '".$filennamepart."'\r\n", FILE_APPEND | LOCK_EX);
-        if ($result === false) {
-            error_log("Failed to write to file: {$fileListPath}"); // Log error internally
-            return false;
+        try{
+            $url = $this->filesURL.$this->quality.strval($this->downloadIterator++).".ts";
+            //echo $url."\r\n";
+            curl_setopt($this->curlHandle, CURLOPT_URL, $url);
+            $fileContent = curl_exec($this->curlHandle);
+            /*
+             * check cURL satus
+             */
+            self::checkCurlStatus();
+            /*
+             * check content
+             */
+            if ($fileContent === false || empty($fileContent)) {
+                Throw New \Exception(__METHOD__."() cURL CONTENT ERROR:".PHP_EOL."No content received for {$url}. It might be an empty file or a download failure.");
+            }
+            $this->filesAvailable++;
         }
-        return true;
+        catch(\Exception $e){
+            self::log(__METHOD__."() FINISH ".$e->getMessage());
+            $curlError = true;
+        }
     }
     
-    private function clean(array $toDelete=[])
+    private function downloadContent()
+    {
+        /*
+         *  Initialize cURL
+         */
+        $fileName = $this->quality.strval($this->downloadIterator++).".ts";
+        $destination = $this->temporaryFileDirectory.$fileName;
+        $url = $this->filesURL.$fileName;
+        //echo $url."\r\n";
+        self::log(__METHOD__."() FILE URL:".PHP_EOL.$url);
+        curl_setopt($this->curlHandle, CURLOPT_URL, $url);
+        /*
+         * Execute cURL and get the content
+         */
+        $fileContent = curl_exec($this->curlHandle);
+        /*
+         * check cURL satus
+         */
+        self::checkCurlStatus();
+        if ($fileContent === false || empty($fileContent)) {
+            Throw New \Exception(__METHOD__."() cURL CONTENT ERROR:".PHP_EOL."No content received for {$url}. It might be an empty file or a download failure.");
+        }
+        $this->filesDownloaded++;
+        $bytesWritten = file_put_contents($destination, $fileContent);
+        if ($bytesWritten !== false) {
+            self::log(__METHOD__."()".PHP_EOL."Successfully downloaded {$url}".PHP_EOL."Saved to {$destination} ({$bytesWritten} bytes).");
+            self::createFFMPEGList($destination);
+        }
+        else {
+            Throw New \Exception(__METHOD__."()Error saving file:".PHP_EOL." {$destination}\r\n");
+        }
+        $this->toDelete[]=$destination;
+    }
+    
+    private function createFFMPEGList(string $filennamepart=''):void
+    {
+        $result = file_put_contents($this->fileList, "file '".$filennamepart."'\r\n", FILE_APPEND | LOCK_EX);
+        if ($result === false) {
+            error_log("Failed to write to file: ".$this->fileList); // Log error internally
+            Throw New \Exception("Failed to write to file: ".$this->fileList);
+        }
+        $this->statusFileListToDelete = true;
+    }
+    
+    private function clean()
     {
         self::log(__METHOD__."()");
-        foreach($toDelete as $del){
+        foreach($this->toDelete as $del){
             unlink($del);
         }
         if(!rmdir($this->temporaryFileDirectory))
@@ -401,6 +417,46 @@ class DownloadFile
         $file = fopen($this->logFile, "w") or die("Unable to open file!");
         fwrite($file, "");
         fclose($file);
+    }
+
+    private function initCurl()
+    {
+        $this->curlHandle = curl_init();
+        /*
+         *  Set cURL options
+         */
+        curl_setopt($this->curlHandle, CURLOPT_HEADER, 1); // Don't return the header
+        curl_setopt($this->curlHandle, CURLOPT_RETURNTRANSFER, true); // Return the transfer as a string
+        /*
+         *  Optional: Set a timeout for the download (in seconds)
+         */
+        curl_setopt($this->curlHandle, CURLOPT_TIMEOUT, 30);
+        /*
+         *  Optional: Follow redirects
+         */
+        curl_setopt($this->curlHandle, CURLOPT_FOLLOWLOCATION, false);
+        /*
+         *  Optional: User-Agent header (some servers require this)
+         */
+        curl_setopt($this->curlHandle, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+        // Optional: Handle SSL certificate issues (use with caution, better to fix certificate problems)
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    }
+    
+    private function checkCurlStatus()
+    {
+        if (curl_errno($this->curlHandle)) {
+            Throw New \Exception(__METHOD__."() cURL ERROR:".PHP_EOL.curl_error($this->curlHandle));
+        }
+        $info = curl_getinfo($this->curlHandle);
+        //var_dump($info);
+        if($info['http_code'] !== 200){
+            Throw New \Exception(__METHOD__."() cURL `".$info['url']."` WRONG HTTP CODE:".PHP_EOL.$info['http_code']);
+        }
+        if($info['content_type'] !== 'video/mp2t'){
+            Throw New \Exception(__METHOD__."() cURL `".$info['url']."` WRONG CONTNET TYPE:".PHP_EOL.$info['content_type']);
+        }
     }
 }
 
